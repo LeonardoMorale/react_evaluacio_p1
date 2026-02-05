377c8119ffe6e87a6d8afd24d8344a5d7844cb1e
LeonardoMorale <leonardo_3141240263@utd.edu.mx>
2026-02-04 21:38:37 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import miFotoLocal from './FOTO_REACT.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="content-box">
+        
+        {}
+        <img src={miFotoLocal} alt="Avatar" className="avatar-img" />
+
+        {}
+        <h1 className="title">Evaluación parcial 1</h1>
+        
+        {}
+        <h2 className="subtitle">Alumno(a): Leonardo Morales Carrera</h2>
+
+        {}
+        <div className="links-container">
+          <a 
+            href="https://www.linkedin.com/in/leonardo-morales-06685036a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app&authuser=2" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="link-text"
+          >
+            LINKED IN DE MI PROFILE
+          </a>
+
+          <a 
+            href="/documentacion.html" 
+            className="link-text"
+          >
+            DOCUMENTACION PARCIAL 1
+          </a>
+        </div>
+
+      </div>
     </div>
   );
 }
